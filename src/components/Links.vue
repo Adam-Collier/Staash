ff1cf32d577da7a5ac21fd7f29f5294c910f9876
Adam Collier <adamcollierart@gmail.com>
2018-08-31 12:01:20 +0100

    conditional links on user authentication

---
+++ src/components/Links.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="nav">
+      <template v-if="this.$store.state.signedIn">
+        <a href="profile">Profile</a>
+        <a @click.prevent="signOut()">Logout</a>
+      </template>
+      <template v-else>
+        <a href="profile">Profile</a>
+        <a href="http://localhost:3001/login">Login</a>
+      </template>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    signOut() {
+      this.$store.commit("signOut");
+    }
+  }
+};
+</script>
+
+<style lang="stylus">
+.nav
+  display: flex
+  justify-content: flex-end
+
+  a
+    padding-right: 10px
+    margin-bottom: 8px
+    cursor: pointer
+</style>
