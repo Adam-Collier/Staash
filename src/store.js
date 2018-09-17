da69ddee0df31485c31126b0fd5b3ac7b6ac9d8f
Adam Collier <adamcollierart@gmail.com>
2018-09-17 10:10:06 +0100

    action to fetch sites, filter and switch the view

--- src/store.js
+++ src/store.js
@@ -5,12 +5,44 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    signedIn: true
+    signedIn: false,
+    sites: "",
+    filterBy: "",
+    desktop: true
   },
   mutations: {
     signOut(state) {
       state.signedIn = false;
+    },
+    signIn(state) {
+      state.signedIn = true;
+    },
+    fetchSites(state) {
+      let token = localStorage.getItem("staashToken");
+      return fetch("http://localhost:3001/api", {
+        headers: new Headers({
+          token: token
+        })
+      })
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+          state.sites = data;
+        })
+        .catch(err => console.log(err));
+    },
+    filterSites(state, text) {
+      state.filterBy = text;
+    },
+    switchView(state) {
+      state.desktop = !state.desktop;
     }
   },
-  actions: {}
+  actions: {
+    getSites(context) {
+      context.commit("fetchSites");
+    }
+  }
 });
