a9e8572c9bc1b9f3231240538f2a5150fbbe3f40
Adam Collier <adamcollierart@gmail.com>
2018-09-17 09:49:25 +0100

    switch links depending on route

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="nav">
       <template v-if="this.$store.state.signedIn">
-        <a href="profile">Profile</a>
-        <a @click.prevent="signOut()">Logout</a>
+        <template v-if="this.$route.name == 'home'">
+          <router-link to="profile">Profile</router-link>
+          <router-link to="/" @click.native="signOut()">Logout</router-link>
+        </template>
+        <template v-else>
+          <router-link to="/">Home</router-link>
+          <router-link to="/" @click.native="signOut()">Logout</router-link>
+        </template>
       </template>
       <template v-else>
-        <a href="profile">Profile</a>
         <a href="http://localhost:3001/login">Login</a>
       </template>
     </div>
@@ -15,6 +20,7 @@
 export default {
   methods: {
     signOut() {
+      localStorage.removeItem("staashToken");
       this.$store.commit("signOut");
     }
   }
