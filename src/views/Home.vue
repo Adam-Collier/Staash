f1f9195d1af44c577d28d3dbc9294bb80b069d1e
Adam Collier <adamcollierart@gmail.com>
2018-09-17 10:11:48 +0100

    show sites if signed in

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="home">
     <Menu />
+    <Sites v-if="signedIn"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Menu from "@/components/Menu.vue";
+import Sites from "@/components/Sites.vue";
 
 export default {
   name: "home",
   components: {
-    Menu
+    Menu,
+    Sites
+  },
+  computed: {
+    signedIn() {
+      return this.$store.state.signedIn;
+    }
   }
 };
 </script>
