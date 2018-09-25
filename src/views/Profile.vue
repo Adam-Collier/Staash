3b6c818c445339a5989dee4a0d3fc07a22b13c67
Adam Collier <adamcollierart@gmail.com>
2018-09-25 11:01:31 +0100

    can now delete single sites

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -12,7 +12,14 @@
       </button>
     </div>
     <div class="sites">
-
+      <h3>Your Sites</h3>
+      <p>
+        All of the sites you have added to Staash. Once you delete a site you won't be able to recover it. 
+      </p>
+      <div class="site" v-for="site in sites" :key="site.id" :data-id="site._id">
+        <h4>{{site.siteTitle}}</h4>
+        <button @click="deleteSites(site._id)">Delete</button>
+      </div>
     </div>
   </div>
 </template>
@@ -25,6 +32,15 @@ export default {
   name: "profile",
   components: {
     Menu
+  },
+  beforeCreate: function() {
+    this.$store.dispatch("getUserSites");
+  },
+  computed: {
+    sites() {
+      let sites = Array.from(this.$store.state.sites);
+      return sites;
+    }
   },
   methods: {
     deleteAccount() {
@@ -46,23 +62,27 @@ export default {
         })
         .catch(err => console.log(err));
     },
-    deleteSites() {
-      fetch("http://localhost:3001/delete-user", {
+    deleteSites(id) {
+      console.log(id);
+      fetch("http://localhost:3001/delete-site", {
         method: "post",
         headers: new Headers({
           Accept: "application/json",
           "Content-Type": "application/json",
-          token: localStorage.getItem("staashToken")
+          token: localStorage.getItem("staashToken"),
+          type: "user"
         }),
-        // get id from data attribute
-        body: JSON.stringify()
+        body: JSON.stringify({ id: id })
+      }).then(() => {
+        this.$store.dispatch("getTeamSites");
+        this.$store.dispatch("getUserSites");
       });
     }
   }
 };
 </script>
 
-<style lang="stylus">
+<style lang="stylus" scoped>
 button
   padding: 10px 20px
   background: #cb2431
@@ -79,4 +99,23 @@ button
   padding: 10px 30px 40px 30px
   margin-top: 50px
   border-radius: 5px
+  margin-bottom: 40px
+
+.sites
+  width: 500px
+  margin: 0 auto 50px auto
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1)
+  padding: 10px 30px 30px 30px
+  border-radius: 5px
+
+.site
+  display: flex
+  flex-direction: row
+  align-items: center
+  justify-content: space-between
+  padding: 5px 0px
+
+  button
+    font-size: 14px
+    font-weight: 500
 </style>
